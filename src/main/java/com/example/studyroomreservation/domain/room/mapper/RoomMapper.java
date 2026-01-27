3b5392a31951e64d2346ce755736273df0fd6f9d
yeseul <@>
2026-01-27 14:26:25 +0900

    feat: 방 예약 규칙 mapper 분리 #25
    - roomMapper/roomRuleMapper 분리

--- src/main/java/com/example/studyroomreservation/domain/room/mapper/RoomMapper.java
+++ src/main/java/com/example/studyroomreservation/domain/room/mapper/RoomMapper.java
@@ -1,41 +1,15 @@
 package com.example.studyroomreservation.domain.room.mapper;
 
-//import com.example.studyroomreservation.domain.room.dto.request.RoomCreateRequest;
-import com.example.studyroomreservation.domain.room.dto.request.RoomRuleCreateRequest;
 import com.example.studyroomreservation.domain.room.dto.response.RoomResponse;
-import com.example.studyroomreservation.domain.room.dto.response.RoomRuleResponse;
 import com.example.studyroomreservation.domain.room.entity.Room;
-import com.example.studyroomreservation.domain.room.entity.RoomImage;
-import com.example.studyroomreservation.domain.room.entity.RoomRule;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-
-import java.util.List;
 
 //componentModel 미지정시 구현체내 별도 인스턴스 접근 호출 필요(RoomMapper INSTANCE = Mappers.getMapper(RoomMapper.class);)
 @Mapper(componentModel = "spring")
 public interface RoomMapper {
     // Entity -> Response DTO (조회)
-    //--질문 : RoomMapper/RoomRuleMapper 분리??
     RoomResponse toResponse(Room room);
-    RoomRuleResponse toRuleResponse(RoomRule rule);
 
     // Request DTO -> Entity (등록)
-    // Room + RoomImage
-//    @Mapping(target = "roomRule", ignore = true) //mapping 다시보기
-//    Room toRoomEntity(RoomCreateRequest request); // Room 엔티티 내부의 RoomRule 연관관계는 서비스에서 주입하거나 ID로 매핑
-//    List<RoomImage> toImageEntities(List<RoomCreateRequest.RoomImageRequest> requests); // List<RoomImageRequest> -> List<RoomImage> 변환
-//    RoomImage toImageEntity(RoomCreateRequest.RoomImageRequest request);
 
-    // RoomRule
-    default RoomRule createRoomRule(RoomRuleCreateRequest request) {
-        if(request == null){
-            return null;
-        }
-        return RoomRule.createRoomRule(
-                request.name(),
-                request.minDurationMinutes(),
-                request.bookingOpenDays()
-        );
-    }
 }
--- src/main/java/com/example/studyroomreservation/domain/room/service/RoomRuleService.java
+++ src/main/java/com/example/studyroomreservation/domain/room/service/RoomRuleService.java
@@ -3,6 +3,7 @@ package com.example.studyroomreservation.domain.room.service;
 import com.example.studyroomreservation.domain.room.dto.response.RoomRuleResponse;
 import com.example.studyroomreservation.domain.room.entity.RoomRule;
 import com.example.studyroomreservation.domain.room.mapper.RoomMapper;
+import com.example.studyroomreservation.domain.room.mapper.RoomRuleMapper;
 import com.example.studyroomreservation.domain.room.repository.RoomRuleRepository;
 import com.example.studyroomreservation.global.exception.BusinessException;
 import com.example.studyroomreservation.global.exception.ErrorCode;
@@ -20,26 +21,27 @@ import java.util.List;
 public class RoomRuleService {
     private final RoomRuleRepository roomRuleRepository;
     private final RoomMapper roomMapper;
+    private final RoomRuleMapper roomRuleMapper;
     private final int PAGE_SIZE = 10;
 
     // 목록 조회
     public Page<RoomRuleResponse> getAllRoomRules(int page) {
         Pageable pageable = PageRequest.of(page, PAGE_SIZE, Sort.by("createdAt").descending());
         return roomRuleRepository.findAll(pageable)
-                .map(roomMapper::toRuleResponse);
+                .map(roomRuleMapper::toRuleResponse);
     }
 
     // 활성화 목록 조회 (룸 등록시)
     public Page<RoomRuleResponse> getActiveRoomRules(int page) {
         Pageable pageable = PageRequest.of(page, PAGE_SIZE, Sort.by("createdAt").descending());
         return roomRuleRepository.findAllByIsActiveTrue(pageable)
-                .map(roomMapper::toRuleResponse);
+                .map(roomRuleMapper::toRuleResponse);
     }
 
     // 상세 조회
     public RoomRuleResponse getRuleDetail(Long id) {
         return roomRuleRepository.findById(id)
-                .map(roomMapper::toRuleResponse)
+                .map(roomRuleMapper::toRuleResponse)
                 .orElseThrow(() -> new BusinessException(ErrorCode.INVALID_REQUEST, "해당 화면을 찾을 수 없습니다. ID: " + id));
     }
 }
