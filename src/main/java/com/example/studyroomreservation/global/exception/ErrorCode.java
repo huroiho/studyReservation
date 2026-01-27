b0b6c8d37732f38af0e194a7ce672395ac668850
[CLOUD6] 박진현 <wlsgus866@gmail.com>
2026-01-27 01:41:21 +0000

    Update ErrorCode.java

--- src/main/java/com/example/studyroomreservation/global/exception/ErrorCode.java
+++ src/main/java/com/example/studyroomreservation/global/exception/ErrorCode.java
@@ -11,14 +11,16 @@ public enum ErrorCode {
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "C002", "잘못된 요청입니다."),
 
     // REFUND
-    REF_POLICY_NAME_REQUIRED(HttpStatus.BAD_REQUEST, "RE001", "정책 이름은 필수입니다."),
-    REF_RULE_REQUIRED(HttpStatus.BAD_REQUEST, "RE002", "최소 하나의 환불 규칙이 필요합니다."),
-    REF_RATE_INVALID(HttpStatus.BAD_REQUEST, "RE003", "환불 비율은 0~100 사이여야 합니다."),
-    REF_RULE_NAME_REQUIRED(HttpStatus.BAD_REQUEST, "RE004", "규칙 이름은 필수입니다."),
-    REF_POLICY_ALREADY_ASSIGNED(HttpStatus.BAD_REQUEST, "RE005", "이미 환불 정책이 할당되어 있습니다."),
-    REF_PAYMENT_REQUIRED(HttpStatus.BAD_REQUEST, "RE006", "환불 대상 결제 정보는 필수입니다."),
-    REF_AMOUNT_INVALID(HttpStatus.BAD_REQUEST, "RE007", "환불 금액은 0원보다 커야 합니다."),
-    REF_POLICY_NOT_FOUND(HttpStatus.NOT_FOUND, "RF008", "환불 정책이 존재하지 않습니다."),
+    REF_POLICY_NAME_REQUIRED(HttpStatus.BAD_REQUEST, "RF001", "정책 이름은 필수입니다."),
+    REF_RULE_REQUIRED(HttpStatus.BAD_REQUEST, "RF002", "최소 하나의 환불 규칙이 필요합니다."),
+    REF_RATE_INVALID(HttpStatus.BAD_REQUEST, "RF003", "환불 비율은 0~100 사이여야 합니다."),
+    REF_RULE_NAME_REQUIRED(HttpStatus.BAD_REQUEST, "RF004", "규칙 이름은 필수입니다."),
+    REF_POLICY_ALREADY_ASSIGNED(HttpStatus.BAD_REQUEST, "RF005", "이미 환불 정책이 할당되어 있습니다."),
+    REF_PAYMENT_REQUIRED(HttpStatus.BAD_REQUEST, "RF006", "환불 대상 결제 정보는 필수입니다."),
+    REF_AMOUNT_INVALID(HttpStatus.BAD_REQUEST, "RF007", "환불 금액은 0원보다 커야 합니다."),
+    REF_POLICY_NAME_DUPLICATE(HttpStatus.CONFLICT, "RF008", "이미 존재하는 정책 이름입니다."),
+    REF_POLICY_NOT_FOUND(HttpStatus.NOT_FOUND, "RF009", "환불 정책이 존재하지 않습니다."),
+
 
     // RESERVATION
     RES_REQUIRED_VALUE_MISSING(HttpStatus.BAD_REQUEST, "R001", "필수 값이 누락되었습니다."),
